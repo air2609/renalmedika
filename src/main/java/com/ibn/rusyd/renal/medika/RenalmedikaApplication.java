9a025ebbb78b8714aaa1c373bc6b53c1b4822be7
Arifullah Ibn Rusyd <ibn.rusyd@gmail.com>
2015-10-27 15:28:16 +0700

    first commit

---
+++ src/main/java/com/ibn/rusyd/renal/medika/RenalmedikaApplication.java
@@ -0,0 +1,12 @@
+package com.ibn.rusyd.renal.medika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenalmedikaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenalmedikaApplication.class, args);
+    }
+}
